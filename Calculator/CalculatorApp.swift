a2bc512f409adda496601c4bb3e04789e985dde0
Miso Lubarda <miso.lubarda@gmail.com>
2022-03-16 20:55:53 -0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Miso Lubarda on 16.03.22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
